28f8a30a37646ac1616d72f76ac5966a4e30a7c1
prashant astola <prashantbastola@gmail.com>
2026-08-27 14:07:55 +0545

    Discount Voucher

--- Assignment/src/Components/Item.jsx
+++ Assignment/src/Components/Item.jsx
@@ -1,10 +1,35 @@
 import React from 'react'
+// import TrashIcon from './Components/Icon'
+
+
+const TrashIcon = () => (
+    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+        <polyline points="3 6 5 6 21 6"></polyline>
+        <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
+        <line x1="10" y1="11" x2="10" y2="17"></line>
+        <line x1="14" y1="11" x2="14" y2="17"></line>
+    </svg>
+);
+
+const MinusIcon = () => (
+    <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+        <line x1="5" y1="12" x2="19" y2="12"></line>
+    </svg>
+);
+
+const PlusIcon = () => (
+    <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+        <line x1="12" y1="5" x2="12" y2="19"></line>
+        <line x1="5" y1="12" x2="19" y2="12"></line>
+    </svg>
+);
+
 
 const Item = ({item , handleItemQuantity, deleteItem }) => {
   return (
 
 
-  <div className="flex">
+   <div className="flex py-6 border-b border-gray-100 last:border-b-0 gap-2 items-center">
             <div className="flex-1 flex items-center gap-2">
                 <img className="w-20 h-20 rounded-xl object-cover" src={item.image} alt="" />
                 <div>
@@ -12,17 +37,17 @@ const Item = ({item , handleItemQuantity, deleteItem }) => {
                     <p className="text-sm text-gray-500 truncate">{item.subtitle}</p>
                 </div>
             </div>
-            <div className="w-[120px] flex gap-2">
-                <button className=" ">-</button>
-                <span className=" h-full align-middle">{item.quantity}</span>
-                <button onClick={()=>{
-                    handleItemQuantity(item.id, item.quantity + 1)
-                }} className=" ">+</button>
+            <div className="w-[120px] flex gap-2 items-center ">
+                <div className="flex border border-gray-200 h-fit rounded-full px-3 py-1 jutify-center gap-5">
+                    <button disabled={item.quantity === 1} onClick={() => handleItemQuantity(item.id, item.quantity - 1)} className="text-gray-500 cursor-pointer"><MinusIcon /></button>
+                    <span className="  align-middle font-semibold text-gray-700">{item.quantity}</span>
+                    <button onClick={() => handleItemQuantity(item.id, item.quantity + 1)} className=" text-gray-500 cursor-pointer"><PlusIcon /></button>
+                </div>
             </div>
-            <div className="w-[80px]">${item.price * item.quantity }</div>
-            <button onClick={()=>{
-                deleteItem(item.id)
-            }}  className="  mb-15 bg-amber-200 p-1 w-[55px] ">Delete</button>
+            <div className="w-[80px] font-semibold">${(item.price * item.quantity).toFixed(2)}</div>
+            <div className="w-[40px]">
+                <button onClick={() => deleteItem(item.id)} className="cursor-pointer text-gray-500"><TrashIcon /></button>
+            </div>
         </div>
 
 
--- Assignment/src/Components/ShoppingCart.jsx
+++ Assignment/src/Components/ShoppingCart.jsx
@@ -2,8 +2,32 @@ import React from 'react'
 import { initialItems } from './data'
 import { useState } from 'react';
 import Item from './Item';
+const discount_percentage = 10;
+
 export const ShoppingCart = () => {
      const [items, setitems] = useState(initialItems);
+     const [inputData, setinputData] = useState("");
+     const [dis, setdis] = useState(100)
+     const [disboolean, setdisboolean] = useState(true)
+
+     function discountData()
+     {
+         if(inputData === 'FREE30')
+         {
+            setdisboolean(false)
+setdis((30 / 100) * SubTotal)
+
+
+         }
+
+         else{
+            alert("Invalid Voucher")
+// setdis(100)
+
+            
+         }
+     }
+
 
     function handleItemQuantity(id, quantity) {
         const newItems = items.map(item => {
@@ -26,26 +50,34 @@ export const ShoppingCart = () => {
         setitems(arrayItems)
     }
 
-   
+
+
+
+   const SubTotal = items.reduce((sum, item) => { return sum + (item.quantity * item.price) }, 0);
+    const DeliveryFee = 100;
+    const total = SubTotal - dis + DeliveryFee
+
+    
 
     
   return (
    <>
-   <main className="min-h-screen ">
+   <main className="min h-screen ">
             <div className=" max-w-6xl mx-auto">
-                <h1 className="text-4xl font-bold mb-12 text-gray-900">Shopping Cart</h1>
-                <div className="flex gap-8">
+                <h1 className="text-4xl font-bold mb-8 mt-10 text-gray-900">Shopping Cart</h1>
+                <div className="  flex gap-8">
                     {/* product */}
                     <div className="w-full border border-gray-300 rounded-xl p-4">
                         <div className="flex text-sm font-semibold pb-6">
                             <h2 className="flex-1">Product code</h2>
-                            <h2 className="w-[120px]">Quantity</h2>
+                            <h2 className="  w-[140px]">Quantity</h2>
                             <h2 className="w-[80px]">Total</h2>
                             <h2 className="w-[40px]">Action</h2>
                         </div>
                         <div>
                             {
-                                items.map(item => <Item handleItemQuantity={handleItemQuantity}  deleteItem={deleteItem}  key={item.id} item={item} />)
+                                items.length > 0   ? items.map(item => <Item handleItemQuantity={handleItemQuantity}  deleteItem={deleteItem}  key={item.id} item={item} />)
+                                : <p>No product Available</p>
                             }
                         </div>
                     </div>
@@ -54,29 +86,45 @@ export const ShoppingCart = () => {
 
                    
 
-                    <div className="w-[380px] ">
+                   <div className="w-[380px] ">
+                        <div className="border border-gray-300 rounded-3xl p-6">
+                            <h2 className="text-lg font-semibold text-gray-900 mb-6">Order Summary</h2>
+                            <div className="flex gap-3">
+                                <input 
+                                onChange={(e)=>{
+           
+                                    setinputData(e.target.value);
+                                    
+                                }}
 
- <h2 className="text-lg font-semibold text-gray-900 mb-6">Order Summary</h2>
-                            <div>
-                                {/* discount input section */}
+                                type="text" placeholder="Discount Voucher" className="border rounded-full focus:outline-gray-500 px-3 border-gray-200" />
+                                <button 
+                                  onClick={discountData}
+                                 className="border border-gray-200 px-6 py-2 rounded-full font-semibold text-sm">Apply</button>
                             </div>
 
-                        <div className="flex justify-between items-center text-sm">
-                                    <span className="text-gray-600">Sub Total</span>
-                                    <span className="font-semibold text-gray-900">100</span>
+                            <div className="mt-8 flex gap-3 flex-col mb-6">
+                                <SummaryData label={"Sub Total"} value={SubTotal.toFixed(2)} />
+                                <SummaryData label={  disboolean === true ?  "Discount (10%)" : "Discount (30%)"} value={ dis.toFixed(2) } />
+                                <SummaryData label={"Delivery fee"} value={DeliveryFee} />
+                                <div className="flex justify-between items-center text-sm border-t border-gray-200 pt-4 mt-3">
+                                    <span className=" text-md text-gray-900 font-semibold">Total</span>
+                                    <span className=" text-gray-900 text-lg font-bold">{`${total.toFixed(2)} USD`}</span>
                                 </div>
-                                <div className="flex justify-between items-center text-sm">
-                                    <span className="text-gray-600">Discount (10%)</span>
-                                    <span className="font-semibold text-gray-900">100</span>
+                            </div>
+                            <div className="flex items-start gap-3 mb-6 bg-gray-50/50 p-3 rounded-xl text-xs text-gray-600">
+                                <div className="shrink-0 mt-0.5 text-gray-400">
+                                 ShiledIcon
                                 </div>
-                                  <div className="flex justify-between items-center text-sm">
-                                    <span className="text-gray-600">Delivery fee</span>
-                                    <span className="font-semibold text-gray-900">100</span>
-                                </div>
-                                  <div className="flex justify-between items-center text-sm">
-                                    <span className="text-gray-600">Total</span>
-                                    <span className="font-semibold text-gray-900">100</span>
-                                </div>
+                                <p>
+                                    90 Day Limited Warranty against manufacturer's defects <a href="#" className="font-semibold underline decoration-gray-300 hover:text-black">Details</a>
+                                </p>
+                            </div>
+
+                            <button 
+                            
+                             className="rounded-full px-6 py-3 w-full bg-gray-900 text-white text-lg font-semibold">Checkout Now</button>
+                        </div>
                     </div>
 
                 </div>
@@ -89,6 +137,15 @@ export const ShoppingCart = () => {
    
    </>
   )
+}
+
+function SummaryData({ label, value }) {
+    return (
+        <div className="flex justify-between items-center text-sm">
+            <span className="text-gray-600">{label}</span>
+            <span className="font-semibold text-gray-900 text-md font-semibold">{`${value} USD`}</span>
+        </div>
+    )
 }
 
 export default ShoppingCart
